27567a22dfdf81770548683220a277ca9b112a83
Paska2005 <114850600+Pashka385@users.noreply.github.com>
2024-01-25 12:27:49 +0900

    Update progress.js

--- src/js/Components/progress.js
+++ src/js/Components/progress.js
@@ -1,1 +1,7 @@
-
+const progress = document.querySelector(".progress");
+window.addEventListener('scroll', () => {
+    const windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercentage = (windowScroll / windowHeight) * 100;
+    progress.style.width = scrollPercentage + '%';
+});
